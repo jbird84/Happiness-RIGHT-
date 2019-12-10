7893478f49290c4fd58578367f07752817a1bdbe
Josh Kinney <joshkinney@kinneykare.com>
2019-12-10 13:40:55 -0600

    Initial Commit

---
+++ Happiness RIGHT?/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Happiness RIGHT?
+//
+//  Created by Kinney Kare on 12/10/19.
+//  Copyright © 2019 Kinney Kare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
